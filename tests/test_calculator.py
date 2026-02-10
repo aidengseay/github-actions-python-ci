24d280199b6ef5d923a4c866fbf67c762d240814
aidengseay <aidengseay@gmail.com>
2026-02-10 09:32:49 -0700

    added calculator and tests

---
+++ tests/test_calculator.py
@@ -0,0 +1,25 @@
+from src.calculator import add, subtract, divide
+import unittest
+
+class TestCalculator(unittest.TestCase):
+
+    def test_add(self):
+        self.assertEqual(add(2, 3), 5)
+        self.assertEqual(add(-1, 1), 0)
+        self.assertEqual(add(-1, -1), -2)
+
+
+    def test_subtract(self):
+        self.assertEqual(subtract(2, 3), -1)
+        self.assertEqual(subtract(-2, 1), 0)
+        self.assertEqual(subtract(5, 4), 1)
+
+
+    def test_divide(self):
+        self.assertEqual(divide(25, 5), 5)
+        self.assertEqual(divide(-25, 5), -5)
+        self.assertEqual(divide(0, 5), 0)
+
+
+if __name__ == '__main__':
+   unittest.main()
